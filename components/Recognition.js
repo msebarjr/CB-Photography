e890913338f24c5db31a9faf2eb15460d4981b4e
Michel Sebar Jr <msebarjr@gmail.com>
2021-09-24 23:47:16 -0400

    Profile img and recognition icons finished w/o responsiveness

--- components/Recognition.js
+++ components/Recognition.js
@@ -1,7 +1,14 @@
-// import styles from "../styles/Recognition.module.css";
+import styles from "../styles/Recognition.module.css";
 
-function Recognition() {
-    return <div>Hello</div>;
+function Recognition(props) {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.icon}>{props.icon}</div>
+            <div className={styles.info}>
+                <span>{props.highlight}</span> {props.text}
+            </div>
+        </div>
+    );
 }
 
 export default Recognition;
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
+import { FaAward, FaStar } from "react-icons/fa";
+import { IoCameraSharp } from "react-icons/io5";
+import { AiFillPicture } from "react-icons/ai";
 
 import Hero from "../components/Hero";
 import Profile from "../components/Profile";
 import Recognition from "../components/Recognition";
 
+import styles from "../styles/About.module.css";
 import aboutHeroImage from "../public/images/landscapes/ferris-wheel-ocean-city-nj-boardwalk.jpg";
 
 export default function About() {
@@ -19,9 +23,32 @@ export default function About() {
                 alt="Boardwalk Ocean City, NY"
                 position="50% 55%"
             />
-            <div className="info">
-                <Profile />
-                <Recognition />
+            <div className="container">
+                <div className={styles.info}>
+                    <Profile />
+                    <div className={styles.recognition}>
+                        <Recognition
+                            icon={<FaAward />}
+                            highlight="9+"
+                            text="Years Of Experience"
+                        />
+                        <Recognition
+                            icon={<FaStar />}
+                            highlight="50+"
+                            text="Satisfied Clients"
+                        />
+                        <Recognition
+                            icon={<IoCameraSharp />}
+                            highlight="100's"
+                            text="Of Photoshoots"
+                        />
+                        <Recognition
+                            icon={<AiFillPicture />}
+                            highlight="1000's"
+                            text="Of Retouched Photos"
+                        />
+                    </div>
+                </div>
             </div>
         </>
     );
